289365c3db20df726c701d22888caf8d1fcb8559
Stephni Houle <stephnihoule@Stephnis-MacBook-Air.local>
2019-11-05 13:14:24 -0500

    views and model files created

--- travelapp/server.js
+++ travelapp/server.js
@@ -16,7 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("welcome");
+});
+
+app.get("/travel/destinations", (req, res) => {
+  res.send("Vamos!");
+});
+
+app.get("/travel/destinations/:id", (req, res) => {
+  res.send("hello!");
 });
 
 app.listen(3000, (req, res) => console.log("listening on PORT 3000!"));
