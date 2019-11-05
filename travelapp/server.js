b299055198f0ddb9a661108373859fa764f51925
Stephni Houle <stephnihoule@Stephnis-MacBook-Air.local>
2019-11-05 12:14:39 -0500

    files set up and connected to server

---
+++ travelapp/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+// DB SETUP
+mongoose.connect("mongodb://localhost:27017/travel", {
+  useNewUrlParser: true
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(3000, (req, res) => console.log("listening on PORT 3000!"));
