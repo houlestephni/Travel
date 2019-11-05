52e002debdb7215c11abb4f4c3fef8e06ae58484
Stephni Houle <stephnihoule@Stephnis-MacBook-Air.local>
2019-11-05 16:33:11 -0500

    schema and seed data created

---
+++ travelapp/models/seed.js
@@ -0,0 +1,25 @@
+const posts = [
+  {
+    title: "Boston, MA",
+    author: "Stephni",
+    description: "Capital of Massachusetts",
+    image:
+      "https://cdn.pixabay.com/photo/2016/08/30/17/12/boston-1631460__480.jpg"
+  },
+  {
+    title: "Scotland",
+    author: "Stephni",
+    description: "Castles, highlands, sheep",
+    image:
+      "https://cdn.pixabay.com/photo/2014/11/21/03/26/scotland-540119_1280.jpg"
+  },
+  {
+    title: "Mexico City",
+    author: "Stephni",
+    description: "history, art, cuisine",
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/26/03/05/mexico-2442582__480.jpg"
+  }
+];
+
+module.exports = posts;
