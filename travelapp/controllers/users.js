676a64d14f7d562bf7b3f7cf9ca5c2303dbe6e0c
Stephni Houle <stephnihoule@stephnis-air.gateway.pace.com>
2019-11-11 21:52:54 -0500

    styling of views pages

--- travelapp/controllers/users.js
+++ travelapp/controllers/users.js
@@ -13,20 +13,16 @@ router.get("/new", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  console.log(currentUser);
   req.body.password = bcrypt.hashSync(
     req.body.password,
-    bcrypt.genSaltSync(10)
-    // () => {
-    //   if (error) {
-    //     res.send("error");
-    //   } else {
-    //     console.log("worked");
-    //   }
-    // }
+    bcrypt.genSaltSync(10),
+    (err, hash) => {
+      if (err) throw err;
+    }
   );
-
   User.create(req.body, (err, createdUser) => {
-    res.redirect("/travel");
+    res.redirect("/");
   });
 });
 
