78de8668437dc6a1b5c6d6a78215c78d29048115
Stephni Houle <stephnihoule@stephnis-air.gateway.pace.com>
2019-11-11 23:30:42 -0500

    updated seed and textarea

--- travelapp/models/seed.js
+++ travelapp/models/seed.js
@@ -1,25 +1,56 @@
 const posts = [
+  // {
+  //   title: "Boston, MA",
+  //   author: "Stephni",
+  //   description: "Capital of Massachusetts",
+  //   image:
+  //     "https://cdn.pixabay.com/photo/2016/08/30/17/12/boston-1631460__480.jpg"
+  // },
+  // {
+  //   title: "Scotland",
+  //   author: "Stephni",
+  //   description: "Castles, highlands, sheep",
+  //   image:
+  //     "https://cdn.pixabay.com/photo/2014/11/21/03/26/scotland-540119_1280.jpg"
+  // },
+  // {
+  //   title: "Mexico City",
+  //   author: "Stephni",
+  //   description: "history, art, cuisine",
+  //   image:
+  //     "https://cdn.pixabay.com/photo/2017/06/26/03/05/mexico-2442582__480.jpg"
+  // }
   {
-    title: "Boston, MA",
+    title: "Salzburg, Austria",
     author: "Stephni",
-    description: "Capital of Massachusetts",
+    description: `Located on the border of Germany, and divided by the Salzach River, Salzburg offers views of the Alps, medieval, and baroque buildings. 
+Why visit in 2020?
+The birthplace of composer Mozart, the city will be celebrating the 100th Salzburg Festival. It is one of the world’s greatest classical music events, featuring opera,  classical music,  and theater. Events will take place all over the city including concerts, plays, and readings.
+`,
     image:
-      "https://cdn.pixabay.com/photo/2016/08/30/17/12/boston-1631460__480.jpg"
+      "https://cdn.pixabay.com/photo/2015/07/12/17/23/salzburg-842215_1280.jpg"
   },
+  { title: "Washington, D.C." },
+  { author: "Stephni" },
   {
-    title: "Scotland",
-    author: "Stephni",
-    description: "Castles, highlands, sheep",
+    description: `The capital of the USA is filled with historical monuments and many museums, including 20 Smithsonian museums and galleries. Visitors can also visit political institutions in DC like the US Capitol building. In 2020, the city will be experiencing a presidential election year and commemorating the 100th anniversary of the 19th amendment—the law that granted women the right to vote. Special exhibits will be held at the National museum of American History and National Museum of Women in the Arts.
+  `,
     image:
-      "https://cdn.pixabay.com/photo/2014/11/21/03/26/scotland-540119_1280.jpg"
-  },
-  {
-    title: "Mexico City",
-    author: "Stephni",
-    description: "history, art, cuisine",
-    image:
-      "https://cdn.pixabay.com/photo/2017/06/26/03/05/mexico-2442582__480.jpg"
+      "https://cdn.pixabay.com/photo/2014/10/07/16/24/us-capitol-477987_1280.jpg"
   }
+  {title:'Cairo, Egypt',
+author: "Stephni",
+description: `The opportunity to explore the pyramids of Giza has long attracted travelers to Egypt but in 2020 there will be another huge attraction to experience. Cairo will soon be home to the world’s largest museum committed to one civilization, when the long-awaited Grand Egyptian Museum (GEM) opens in 2020. The main attraction will be the first exhibition of the full tomb collection of King Tutankhamun. `,
+image:'https://cdn.pixabay.com/photo/2017/01/14/20/55/cairo-1980350_1280.jpg'},
+{title:'Galway, Ireland',
+author: 'Stephni',
+description: `County Galway, on the west coast of Ireland has picturesque scenery and historical sights, including the Aran Islands. Next year will be a great time to visit as Galway has been named the European Capital of Culture for 2020. The city will be highlighting the region’s art, music, theatre, and dance with events and celebrations. The colorful city streets of Galway are home to many old pubs and traditional music performed nightly.`, 
+image:'https://cdn.pixabay.com/photo/2017/07/29/18/29/abbey-2552326_1280.jpg'},
+{title:'Bonn, Germany',
+author: 'Stephni',
+description: `Once the capital of west Germany, Bonn is back in the spotlight as the city gets ready to celebrate Beethoven’s 250th birthday. As the birthplace of Beethoven, the city celebrates with concerts all year-round from world famous orchestras, soloists, and conductors. There will be plenty of performances and concerts from local musicians as well.
+`,
+image: 'https://cdn.pixabay.com/photo/2018/09/12/22/02/church-3673335_1280.jpg'}
 ];
 
 module.exports = posts;
